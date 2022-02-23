e21a59d9ea8aaf215f2ff4c81066efa3b0c3b840
sjuyal12 <sakshijuyal12690@gmail.com>
2022-02-23 13:41:23 -0400

    Adding an alternative way of finding the first unique character in a string

--- src/main/scala/Question1.scala
+++ src/main/scala/Question1.scala
@@ -1,10 +1,10 @@
 object Question1{
 
   def main(args: Array[String]): Unit = {
-    println(firstUniqueCharacter("leetcode"))
+    println(firstUniqueCharacter2("leetcode"))
   }
 
-  def firstUniqueCharacter(str: String): Int = {
+  def firstUniqueCharacter1(str: String): Int = {
     if (str.length > 1){
       val keys = str.zipWithIndex.groupBy(_._2).filter(_._2.length == 1)
       if (keys.size > 0) {
@@ -13,6 +13,18 @@ object Question1{
         }.sorted.head
       } else -1
     } else 0
+  }
+
+  def firstUniqueCharacter2(str: String): Int = {
+    if (str.length > 1){
+      val map = str.zipWithIndex.groupBy(_._1)
+      val seq1 = map.filter(_._2.length == 1).keys.toSeq
+      val ch = seq1.foldLeft(seq1(0)) {
+        (count, ch) => if (str.indexOf(count) < str.indexOf(ch)) count
+        else ch
+      }
+      str.indexOf(ch)
+    } else -1
   }
 
 }
